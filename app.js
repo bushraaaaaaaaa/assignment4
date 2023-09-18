c4d6dfcea11f637339ee7570046c23eeb2d04789
bushraaaaaaaaa <bushraqadir78@gmail.com>
2023-09-19 03:08:46 +0500

    two files added

---
+++ app.js
@@ -0,0 +1,134 @@
+// question 3
+
+// for(var i=1;i<=10;i++){
+//     console.log(i);
+// }
+
+// question4:
+// var table=+prompt("enter number  to show its multiplication table");
+// var tablelength=+prompt("enter length of the table");
+// for(var i=1;i<=tablelength;i++){
+//     document.write(table + "x" + i + "=" + table*i + "<br>");
+// }
+
+// question5
+// var arr = ["banana", "apple", "orange", "mango"]
+// for (i = 0; i <= arr.length; i++) {
+//     console.log("elements of " + arr[i] + " "  +"at" + " " + i);
+// }
+
+// question6
+// document.write("counting:")
+// for (var i = 1; i <= 15; i++) {
+//     document.write("<br>" + i);
+// }
+// document.write("reverse counting:")
+// for (var i = 15; i >= 1; i--) {
+//     document.write("<br>" + i);
+// }
+// document.write("even:")
+// for (var i = 2; i <= 15; i = i + 2) {
+//     document.write("<br>" + i);
+// }
+// document.write("odd:")
+// for (var i = 1; i <= 15; i = i + 2) {
+//     document.write("<br>" + i);
+// }
+// document.write("series:")
+// for (var i = 2; i <= 15; i = i + 2) {
+//     document.write("<br>" + i + "k");
+// }
+// question 10
+// for(var i=5;i<=100;i=i+5){
+//     document.write(i + ",");
+// }
+// ****************************HOME ASSIGNMENT*****************************************88
+// Question1
+// for(var i=0;i<=10;i++){
+//     document.write(i);
+// }
+// question 2
+// for(var i=0;i<=12;i++){
+//     document.write(i);
+// }
+// question 3
+// 1-round bracket
+// 2-semicolon
+// 3-semicolon
+// 4-roundbracket
+// 5-curly bracket
+// for (var i = 0 ;i <= 4; i++){
+
+// }
+// question 4:
+// for(var counter=0;counter<100;counter++){
+//     document.write(counter);
+// // }
+// // question 5:
+// for(var i=3;i>0;i--){
+//     document.write(i + "<br>");
+// }
+// question 6:
+// // var arr = ["banana", "apple", "orange", "mango"]
+// // for (i = 0; i <= arr.length; i++) {
+// //     console.log("elements of " + arr[i] + " "  +"at" + " " + i);
+// // }
+// question 7
+// var flag=true;
+// question 8
+// var arr = ["cat", "dogs", "rabbits", "chicks"]
+// for (i = 0; i <= arr.length; i++) {
+//     console.log(arr[i]);
+// }
+// Q9
+// for (var i = 1; i <= 10; i++) {
+//     if (i === 2) {
+//         alert("Counter value is: " + (i - 1));
+//         break;
+//     }
+// }
+
+// Q10
+// var userNames = ["hawa", "bushra", "naila", "laiba"];
+
+// var firstName=prompt("Enter the first name");
+
+// for(i=0;i<userNames.length;i++){
+//     if(firstName===userNames[i]){
+//         alert("Found");
+//         break;
+//     }
+//     else{
+//         alert ("Please write correct user name");
+//         break;
+//     }
+// }
+
+// Q11
+// var list = ["hawa", "bushra", "naila", "laiba"];
+
+// var userInput=prompt("Enter the first name");
+
+// var matchFound = false;
+// for (var i = 0; i < list.length; i++) {
+// if (userInput === list[i]) {
+// alert("Match found");
+// matchFound = true; 
+// break;
+// }
+// else{
+//     alert ("Please write correct user name");
+//     break;
+//     }
+// }
+
+// Q12
+// var firstArr = ["a","b","c","d","e","f"];
+// var secondArr = [1, 2, 3, 4, 5, 6];
+
+// for (var i = 0; i < firstArr.length; i++) {
+//     for (var j = 0; j < secondArr.length; j++) {
+//         console.log(firstArr[i] + secondArr[j]);
+//     }
+// 
+
